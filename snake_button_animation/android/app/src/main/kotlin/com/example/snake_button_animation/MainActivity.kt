d14a388ac288e90c1c8911bd67e7e7549c30784e
StarKzH <88117244+guilhermeSchiochet@users.noreply.github.com>
2023-07-01 19:32:50 -0300

    Criado novo projeto flutter

---
+++ snake_button_animation/android/app/src/main/kotlin/com/example/snake_button_animation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.snake_button_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ snake_button_animation/lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:snake_button_animation/presentation/app.dart';
+
+void main() => runApp(const App());
